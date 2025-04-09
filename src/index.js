c608cdef18d0c3d0bb7ea687886120198cea18c8
Jonathan <jonathancarpena99@gmail.com>
2025-04-09 16:58:23 -0700

    :construction: Updated port

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const connectDB = require("./config/db.js");
 require("dotenv").config();
 
 connectDB();
-
+const PORT = process.env.PORT || 4000; // Render sets process.env.PORT automatically
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,6 +12,4 @@ app.use(cors());
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/user", require("./routes/userRoutes"));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on ${process.env.PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
